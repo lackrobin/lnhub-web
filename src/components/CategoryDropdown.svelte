8fca3487fbb2212d53ae8dcb3688d705d9849aa2
robin <rolator@protonmail.ch>
2022-05-09 13:10:14 +0200

    add indicator for selected category

--- src/components/CategoryDropdown.svelte
+++ src/components/CategoryDropdown.svelte
@@ -1,12 +1,14 @@
 <script>
+    import { page } from '$app/stores';
 	export let categories;
+    
 </script>
 
 <div class="dropdown">
 	<label tabindex="0" class="btn" for="categories-dropdown">Category</label>
 	<ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52" id="categories-dropdown">
 		{#each categories.categories as data}
-			<li><a sveltekit:prefetch href="/posts/{data.name}">{data.name}</a></li>
+			<li><a sveltekit:prefetch href="/posts/{data.name}" class="{$page.url.pathname.includes(encodeURIComponent(data.name))?"font-bold": ""}">{data.name}</a></li>
 		{/each}
 	</ul>
 </div>
