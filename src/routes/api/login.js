ccfeded743d91567b7f3df46533f8d46ab3df444
robin <rolator@protonmail.ch>
2022-05-15 22:18:17 +0200

    removed unused vars in login api

--- src/routes/api/login.js
+++ src/routes/api/login.js
@@ -1,13 +1,11 @@
 const target = "https://github.com/login/oauth/authorize"
 const clientID = process.env.GH_CLIENT_ID
-export const get = async ({request}) => {
-    const sessionID = "1234"
-
+export const get = async () => {
     return {
         status: 302,
         headers: {
             location:
-                `${target}?client_id=${clientID}&session_id=${sessionID}`
+                `${target}?client_id=${clientID}`
         }
     }
 }
